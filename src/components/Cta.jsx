40b7a84aec853af2ff8e539f0f25698ce366f522
unknown <hmy1304@kbu.ac.kr>
2026-02-24 14:41:46 +0900

    update

--- src/components/Cta.jsx
+++ src/components/Cta.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import categories from '../util/cta'
+import "./styles/Cta.scss"
 
 const Cta = () => {
   return (
-    <div>Cta</div>
+    <div className='inner cta-inner'>
+      <ul className="cta-list">
+        {categories.map((c)=>(
+          <a href={c.id}>
+            <div 
+            className="img-wrap" 
+            style={{backgroundImage:`url(${c.img.src})`}}>
+
+            </div>
+            <p>
+              {c.name}
+            </p>
+          </a>
+        ))}
+      </ul>
+
+    </div>
   )
 }
 
